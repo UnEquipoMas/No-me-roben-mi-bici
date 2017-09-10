5f47729a73d68a6541c552b6fa6d0132b9acb455
Juan Esteban Caicedo <jecaicedop@unal.edu.co>
2017-09-09 20:08:27 -0500

    Fixing

--- db/migrate/20170907185000_add_columns_to_user.rb
+++ db/migrate/20170907185000_add_columns_to_user.rb
@@ -1,7 +1,7 @@
-class AddColumnsToUser < ActiveRecord::Migration[5.1]
+class AddFieldsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :nick, :string
     add_column :users, :name, :string
+    add_column :users, :nick, :string
     add_column :users, :phone, :integer
     add_column :users, :photo, :binary
   end
