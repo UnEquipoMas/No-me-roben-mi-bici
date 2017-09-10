e23d5a0361b1abe45655bff83198b181c77801c2
Juan Esteban Caicedo <jecaicedop@unal.edu.co>
2017-09-09 19:50:01 -0500

    Adding columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
+class AddFieldsToUsers < ActiveRecord::Migration
+  def change
+    add_column :user, :name, :string
+    add_column :user, :apellido, :string
+    add_column :user, :nick, :string
+    add_column :user, :telefono, :string
+    add_column :user, :photo, :string
+  end
+end
