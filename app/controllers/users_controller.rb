62ca4a06740b04dc9f7988e2c4205900ad5a1be3
Juan Esteban Caicedo <jecaicedop@unal.edu.co>
2017-09-09 20:29:01 -0500

    Updating controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :nick, :email, :phone, :password,
+                                   :password_confirmation, :photo)
+    end
 end
