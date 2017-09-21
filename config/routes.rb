a60449702cd11a573ef03de6db14d73c266266d3
dahoyosr <dahoyosr@unal.edu.co>
2017-09-20 20:06:26 -0500

    contactenos

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  #get  '/contact', to: 'static_pages#contact'
+  post '/contact', to: 'messages#create'
+  get '/contact', to: 'messages#new'
+
 
   resources :users
   root 'static_pages#home'
