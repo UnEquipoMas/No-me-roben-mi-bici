89fd52b7743a87bdf819cfdde51976b6ba6caeee
wolfstain <dahoyosr@unal.edu.co>
2017-09-24 17:06:55 +0000

    c9 facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,7 +251,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "128647204453664", "f469e8f43a71a0cb8e4695d111995a4c", callback_url: "http://localhost:3000/users/auth/facebook/callback"
+  config.omniauth :facebook, "128647204453664", "f469e8f43a71a0cb8e4695d111995a4c", callback_url: "https://rails-tutorial-dahoyosr.c9users.io/users/auth/facebook/callback" #callback_url: "http://localhost:3000/users/auth/facebook/callback"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20170924152000) do
 
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
