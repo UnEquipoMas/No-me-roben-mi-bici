adcc0ab33ed166fe9b1fb2e5799c4b178c195d53
wolfstain <dahoyosr@unal.edu.co>
2017-10-04 20:23:12 +0000

    sidebar_admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927062835) do
+ActiveRecord::Schema.define(version: 20171004200845) do
 
   create_table "brands", force: :cascade do |t|
     t.string "description", null: false
@@ -142,6 +142,7 @@ ActiveRecord::Schema.define(version: 20170927062835) do
     t.string "nick"
     t.integer "phone"
     t.string "photo"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
