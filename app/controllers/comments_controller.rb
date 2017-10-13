335642af78159c62b0cd166b71ac0ef9f596049c
wolfstain <dahoyosr@unal.edu.co>
2017-10-13 11:48:12 +0000

    comentarios

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   before_action :find_report
-  before_action :find_comment, only: [:destroy]
+  before_action :find_comment, only: [:destroy, :edit , :update,:comment_owner]
+  before_action :comment_owner, only: [:destroy, :edit , :update]
   
   
   
@@ -21,6 +22,18 @@ class CommentsController < ApplicationController
     redirect_to report_path(@report)
   end
   
+  def edit
+    
+  end
+  
+  def update
+    if @comment.update(params[:comment].permit(:content))
+      redirect_to report_path(@report)
+    else
+      render 'edit'
+    end
+  end
+  
   private
   
   def find_report
@@ -29,6 +42,14 @@ class CommentsController < ApplicationController
   
   def find_comment
   @comment= @report.comments.find(params[:id])
+  end
+  
+  def comment_owner
+    unless current_user == @comment.user
+      flash[:notice] = 'No puedes acceder a esta sección'
+      redirect_to @report
+    end
+    
   end
   
 end
