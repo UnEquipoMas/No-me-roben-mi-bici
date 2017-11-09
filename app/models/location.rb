280a899027187f098139eec5cd12d51d57dfd9d3
wolfstain <dahoyosr@unal.edu.co>
2017-11-09 15:04:07 +0000

    quitar locations

--- app/models/location.rb
+++
@@ -1,8 +0,0 @@
-class Location < ActiveRecord::Base
-  geocoded_by :address
-  after_validation :geocode, :if => :address_changed?
-  def location_params
-    params.require(:location).permit(:address, :latitude, :longitude)
-  end
-end
-
